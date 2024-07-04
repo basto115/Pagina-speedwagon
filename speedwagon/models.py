2583ec502cf0b1aac3e90c2aafbe8cac2fee7557
basto115 <bastianasd3@gmail.com>
2024-07-04 17:28:24 -0400

    modelos para bd

--- speedwagon/models.py
+++ speedwagon/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=25)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100, unique=True, blank=True, null=True)
+    user = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=30)
+    
+def __str__(self):
+    return (
+        str(self.nombre) 
+        + " "
+        + str(self.apellido)
+        + " "
+        + str(self.user)
+    )
+
