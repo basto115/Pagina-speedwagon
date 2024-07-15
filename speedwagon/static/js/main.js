ae76666bd6b044785f91bd425d80b2d913c578ef
basto115 <bastianasd3@gmail.com>
2024-07-15 11:25:35 -0400

    js para carrito

---
+++ speedwagon/static/js/main.js
@@ -0,0 +1,234 @@
+
+const productos = [
+    {
+        id: "oferton-01",
+        titulo: "peluche rei chikita",
+        imagen: "./static/imagenes/chikita.png",
+        categoria: {
+            nombre: "oferton",
+            id: "oferton"
+        },
+        precio: 15000
+    },
+
+    {
+        id: "oferton-02",
+        titulo: "Nendoroid Kishibe Rohan",
+        imagen: "./static/imagenes/rohan.png",
+        categoria: {
+            nombre: "oferton",
+            id: "oferton"
+        },
+        precio: 65000
+    },
+
+    {
+        id: "oferton-03",
+        titulo: "Chainsaw Man Tomo 12",
+        imagen: "./static/imagenes/asa.jpg",
+        categoria: {
+            nombre: "oferton",
+            id: "oferton"
+        },
+        precio: 10000
+    },
+
+    {
+        id: "bandai-01",
+        titulo: "Figura Luffy Gear 5",
+        imagen: "./static/imagenes/luffyg5.png",
+        categoria: {
+            nombre: "bandai",
+            id: "bandai"
+        },
+        precio: 30000
+    },
+
+    {
+        id: "bandai-02",
+        titulo: "Figura Ippo Makunouchi",
+        imagen: "./static/imagenes/ippo.jpg",
+        categoria: {
+            nombre: "bandai",
+            id: "bandai"
+        },
+        precio: 40000
+    },
+
+    {
+        id: "bandai-03",
+        titulo: "Figura Berserk",
+        imagen: "./static/imagenes/berserk.jpg",
+        categoria: {
+            nombre: "bandai",
+            id: "bandai"
+        },
+        precio: 60000
+    },
+
+    {
+        id: "figura-01",
+        titulo: "Figura a escala Kiluua Zoldyck",
+        imagen: "./static/imagenes/killua.jpg",
+        categoria: {
+            nombre: "figura",
+            id: "figura"
+        },
+        precio: 80000
+    },
+
+    {
+        id: "figura-02",
+        titulo: "Figura a escala Mitsuri Kanroji",
+        imagen: "./static/imagenes/mitsuri.png",
+        categoria: {
+            nombre: "figura",
+            id: "figura"
+        },
+        precio: 50000
+    },
+
+    {
+        id: "figura-03",
+        titulo: "Figura a escala Eva 01",
+        imagen: "./static/imagenes/eva.png",
+        categoria: {
+            nombre: "figura",
+            id: "figura"
+        },
+        precio: 90000
+    },
+
+    {
+        id: "shonen-01",
+        titulo: "Tomo 100 One Piece",
+        imagen: "./static/imagenes/opmanga.png",
+        categoria: {
+            nombre: "shonen",
+            id: "shonen"
+        },
+        precio: 12000
+    },
+
+    {
+        id: "shonen-02",
+        titulo: "Tomo 14 Hunter x Hunter",
+        imagen: "./static/imagenes/hxh.png",
+        categoria: {
+            nombre: "shonen",
+            id: "shonen"
+        },
+        precio: 10000
+    },
+
+    {
+        id: "shonen-03",
+        titulo: "Tomo 12 Chainsaw Man",
+        imagen: "./static/imagenes/asa.jpg",
+        categoria: {
+            nombre: "shonen",
+            id: "shonen"
+        },
+        precio: 9000
+    },
+
+    {
+        id: "seinen-01",
+        titulo: "Tomo 10 Jojolion",
+        imagen: "./static/imagenes/jojomanga.png",
+        categoria: {
+            nombre: "seinen",
+            id: "seinen"
+        },
+        precio: 12000
+    },
+
+    {
+        id: "seinen-02",
+        titulo: "Tomo 30 Vagabond",
+        imagen: "./static/imagenes/vagabond.jpg",
+        categoria: {
+            nombre: "seinen",
+            id: "seinen"
+        },
+        precio: 12000
+    },
+
+    {
+        id: "seinen-03",
+        titulo: "Tomo 3 Berserk Edicion Deluxe",
+        imagen: "./static/imagenes/tomoberserk.jpg",
+        categoria: {
+            nombre: "seinen",
+            id: "seinen"
+        },
+        precio: 50000
+    },
+
+    {
+        id: "ropa-01",
+        titulo: "Polera Guts",
+        imagen: "./static/imagenes/poleraberserk.png",
+        categoria: {
+            nombre: "ropa",
+            id: "ropa"
+        },
+        precio: 15000
+    },
+
+    {
+        id: "ropa-02",
+        titulo: "Poleron Death Note",
+        imagen: "./static/imagenes/deathnote.png",
+        categoria: {
+            nombre: "ropa",
+            id: "ropa"
+        },
+        precio: 25000
+    },
+
+    {
+        id: "ropa-03",
+        titulo: "Polera Gojo Satoru",
+        imagen: "./static/imagenes/gojo.png",
+        categoria: {
+            nombre: "ropa",
+            id: "ropa"
+        },
+        precio: 15000
+    },
+
+    {
+        id: "peluches-01",
+        titulo: "Peluche Ellie Chikita",
+        imagen: "./static/imagenes/ellie.png",
+        categoria: {
+            nombre: "peluches",
+            id: "peluches"
+        },
+        precio: 18000
+    },
+
+    {
+        id: "peluches-02",
+        titulo: "Peluche Mob Chikito",
+        imagen: "./static/imagenes/mob.png",
+        categoria: {
+            nombre: "peluches",
+            id: "peluches"
+        },
+        precio: 16000
+    },
+
+    {
+        id: "peluches-03",
+        titulo: "Peluche Jotaro Chikito",
+        imagen: "./static/imagenes/jotaro.png",
+        categoria: {
+            nombre: "peluches",
+            id: "peluches"
+        },
+        precio: 16000
+    },
+
+]
